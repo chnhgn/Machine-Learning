dfbb61b569f9373ddaf32c76cc7b55c37ce330d0
Eddy Hu <guoning.hu@sas.com>
2018-07-22 16:07:00 +0800

    Adding user online features

--- code/ali/o2o/extract.py
+++ code/ali/o2o/extract.py
@@ -205,6 +205,131 @@ class extract(object):
         if total_data_number1 - total_data_number2 != 0:
             raise RuntimeError('Data lost')
         
+    def user_online_features(self):
+        # Total user number
+        print('Total online user number is %d' % len(self.df_online['User_id'].drop_duplicates()))
+
+        # [feature 10]
+        df_ft8_1 = self.df_offline[(self.df_offline.Date_received != 'null')][['User_id', 'Date_received']]
+        df_ft8_1 = df_ft8_1.groupby(['User_id'], as_index=False)['Date_received'].count()
+        df_ft8_1.rename(columns={'Date_received':'offline_cp_record'}, inplace=True)
+        
+        df_ft8_2 = self.df_online[(self.df_online.Date_received != 'null')][['User_id', 'Date_received']]
+        df_ft8_2 = df_ft8_2.groupby(['User_id'], as_index=False)['Date_received'].count()
+        df_ft8_2.rename(columns={'Date_received':'online_cp_record'}, inplace=True)
+        
+        df_ft8_3 = self.df_online[['User_id']].drop_duplicates()
+        df_ft8 = pd.merge(pd.merge(df_ft8_3, df_ft8_2, how='left', on=['User_id']), df_ft8_1, how='left', on=['User_id'])
+        df_ft8.fillna(0, inplace=True)
+        df_ft8['offline_cp_record_in_online_and_offline'] = df_ft8.apply(lambda x:x[1]/(x[1]+x[2]) if x[1]+x[2] != 0 else 0, axis=1)
+        df_ft8 = df_ft8[['User_id', 'offline_cp_record_in_online_and_offline']]
+        
+        # [feature 9]
+        df_ft7_1 = self.df_offline[(self.df_offline.Date != 'null') & (self.df_offline.Coupon_id != 'null')][['User_id', 'Date']]
+        df_ft7_1 = df_ft7_1.groupby(['User_id'], as_index=False)['Date'].count()
+        df_ft7_1.rename(columns={'Date':'offline_cp_used_num'}, inplace=True)
+        
+        df_ft7_2 = self.df_online[(self.df_online.Date != 'null') & (self.df_online.Coupon_id != 'null')][['User_id', 'Date']]
+        df_ft7_2 = df_ft7_2.groupby(['User_id'], as_index=False)['Date'].count()
+        df_ft7_2.rename(columns={'Date':'online_cp_used_num'}, inplace=True)
+        
+        df_ft7_3 = self.df_online[['User_id']].drop_duplicates()
+        df_ft7 = pd.merge(pd.merge(df_ft7_3, df_ft7_2, how='left', on=['User_id']), df_ft7_1, how='left', on=['User_id'])
+        df_ft7.fillna(0, inplace=True)
+        df_ft7['offline_cp_used_num_in_online_and_offline'] = df_ft7.apply(lambda x:x[1]/(x[1]+x[2]) if x[1]+x[2] != 0 else 0, axis=1)
+        df_ft7 = df_ft7[['User_id', 'offline_cp_used_num_in_online_and_offline']]
+        
+        # [feature 8]
+        df_ft6_1 = self.df_offline[(self.df_offline.Date == 'null')][['User_id', 'Date']]
+        df_ft6_1 = df_ft6_1.groupby(['User_id'], as_index=False)['Date'].count()
+        df_ft6_1.rename(columns={'Date':'offline_no_consume_num'}, inplace=True)
+        
+        df_ft6_2 = self.df_online[(self.df_online.Date == 'null')][['User_id', 'Date']]
+        df_ft6_2 = df_ft6_2.groupby(['User_id'], as_index=False)['Date'].count()
+        df_ft6_2.rename(columns={'Date':'online_no_consume_num'}, inplace=True)
+        
+        df_ft6_3 = self.df_online[['User_id']].drop_duplicates()
+        df_ft6 = pd.merge(pd.merge(df_ft6_3, df_ft6_2, how='left', on=['User_id']), df_ft6_1, how='left', on=['User_id'])
+        df_ft6.fillna(0, inplace=True)
+        df_ft6['offline_no_consume_num_in_online_and_offline'] = df_ft6.apply(lambda x:x[1]/(x[1]+x[2]) if x[1]+x[2] != 0 else 0, axis=1)
+        df_ft6 = df_ft6[['User_id', 'offline_no_consume_num_in_online_and_offline']]
+        
+        
+        # [feature 7]
+        df_ft5_1 = self.df_online[['User_id']].drop_duplicates()
+        
+        df_ft5_2 = self.df_online[(self.df_online.Coupon_id != 'null')][['User_id', 'Coupon_id']]
+        df_ft5_2 = df_ft5_2.groupby(['User_id'], as_index=False)['Coupon_id'].count()
+        
+        df_ft5_3 = self.df_online[(self.df_online.Coupon_id != 'null') & (self.df_online.Date != 'null')][['User_id', 'Date']]
+        df_ft5_3 = df_ft5_3.groupby(['User_id'], as_index=False)['Date'].count()
+        
+        df_ft5_4 = pd.merge(df_ft5_2, df_ft5_3, how='left', on=['User_id'])
+        df_ft5 = pd.merge(df_ft5_1, df_ft5_4, how='left', on=['User_id'])
+        df_ft5.fillna(0, inplace=True)
+        df_ft5['Coupon_id'] = df_ft5.Coupon_id.astype('float')
+        df_ft5['Date'] = df_ft5.Date.astype('float')
+        df_ft5['online_cp_used_rate'] = df_ft5.apply(lambda x:x[-1]/x[-2] if x[-2] != 0 else 0, axis=1)
+        df_ft5 = df_ft5[['User_id', 'online_cp_used_rate']]
+        
+        # [feature 6]
+        df_ft4_1 = self.df_online[(self.df_online.Coupon_id != 'null') & (self.df_online.Date != 'null')][['User_id', 'Date']]
+        df_ft4_1 = df_ft4_1.groupby(['User_id'], as_index=False)['Date'].count()
+        df_ft4_1.rename(columns={'Date':'online_cp_used_num'}, inplace=True)
+        
+        df_ft4_2 = self.df_online[['User_id']].drop_duplicates()
+        
+        df_ft4 = pd.merge(df_ft4_2, df_ft4_1, how='left', on=['User_id'])
+        df_ft4.fillna(0, inplace=True)
+        
+        # [feature 5]
+        df_ft3_1 = self.df_online[(self.df_online.Action.isin(['0', '2']))][['User_id', 'Action']]
+        df_ft3_1 = df_ft3_1.groupby(['User_id'], as_index=False)['Action'].count()
+        df_ft3_1.rename(columns={'Action':'online_no_consume_num'}, inplace=True)
+        
+        df_ft3_2 = self.df_online[['User_id']].drop_duplicates()
+        df_ft3 = pd.merge(df_ft3_2, df_ft3_1, how='left', on=['User_id'])
+        df_ft3.fillna(0, inplace=True)
+        
+        # [feature 2,3,4]
+        df_ft2_1 = self.df_online[['User_id', 'Action']]
+        df_ft2_1 = df_ft2_1.groupby(['User_id'], as_index=False)['Action'].count()
+        df_ft2_1.rename(columns={'Action':'total_action_num'}, inplace=True)
+        df_ft2_1.set_index(['User_id'], inplace=True)
+        
+        df_ft2_2 = self.df_online[(self.df_online.Action == '0')][['User_id', 'Action']]
+        df_ft2_2 = df_ft2_2.groupby(['User_id'], as_index=False)['Action'].count()
+        df_ft2_2.rename(columns={'Action':'hit_action_num'}, inplace=True)
+        df_ft2_2.set_index(['User_id'], inplace=True)
+        
+        df_ft2_3 = self.df_online[(self.df_online.Action == '1')][['User_id', 'Action']]
+        df_ft2_3 = df_ft2_3.groupby(['User_id'], as_index=False)['Action'].count()
+        df_ft2_3.rename(columns={'Action':'buy_action_num'}, inplace=True)
+        df_ft2_3.set_index(['User_id'], inplace=True)
+        
+        df_ft2_4 = self.df_online[(self.df_online.Action == '2')][['User_id', 'Action']]
+        df_ft2_4 = df_ft2_4.groupby(['User_id'], as_index=False)['Action'].count()
+        df_ft2_4.rename(columns={'Action':'get_cp_action_num'}, inplace=True)
+        df_ft2_4.set_index(['User_id'], inplace=True)
+        
+        df_ft2 = pd.concat([df_ft2_1, df_ft2_2, df_ft2_3, df_ft2_4], axis=1)
+        df_ft2.reset_index(inplace=True)
+        df_ft2.rename(columns={'index':'User_id'}, inplace=True)
+        df_ft2.fillna(0, inplace=True)
+        df_ft2['hit_action_num'] = df_ft2.hit_action_num.astype('float')
+        df_ft2['buy_action_num'] = df_ft2.buy_action_num.astype('float')
+        df_ft2['get_cp_action_num'] = df_ft2.get_cp_action_num.astype('float')
+        df_ft2['total_action_num'] = df_ft2.total_action_num.astype('float')
+        df_ft2['online_hit_action_rate'] = df_ft2.apply(lambda x:x[2]/x[1] if x[1] != 0 else 0, axis=1)
+        df_ft2['online_buy_action_rate'] = df_ft2.apply(lambda x:x[3]/x[1] if x[1] != 0 else 0, axis=1)
+        df_ft2['online_get_cp_action_rate'] = df_ft2.apply(lambda x:x[4]/x[1] if x[1] != 0 else 0, axis=1)
+        df_ft2 = df_ft2[['User_id', 'online_hit_action_rate', 'online_buy_action_rate', 'online_get_cp_action_rate']]
+        
+        # [Feature 1]
+        df_ft1_1 = self.df_online[['User_id', 'Action']]
+        df_ft1 = df_ft1_1.groupby(['User_id'], as_index=False)['Action'].count()
+        df_ft1.rename(columns={'Action':'online_action_times'}, inplace=True)
+        
         
 
 
@@ -212,7 +337,8 @@ class extract(object):
 
 if __name__ == '__main__':
     extr = extract('C:\\scnguh\\datamining\\o2o')
-    extr.user_offline_features()
+#     extr.user_offline_features()
+    extr.user_online_features()
     
     
     
